dc05ade0d5e8bab467f3ab0b22c52a829bf777d2
Boryana Dimitrova <b.dimitrova97@gmail.com>
2023-07-04 17:29:03 +0300

    Start profile page

--- src/controller/creaturesController.js
+++ src/controller/creaturesController.js
@@ -38,10 +38,18 @@ router.get('/:id/details', async (req, res) => {
     let creaturesData = await creatures.toObject();
 
     let isOwner = creaturesData.owner == req.user?._id;
+    let creaturesOwner = await creaturesServices.findOwner(creatures.owner).lean();
+    let creatureInfo = creaturesData.voted;
+
+    let emails = [];
+    creatureInfo.forEach(x => emails.push(x.email));
+    emails.join(', ');
+    // console.log(creatureInfo);
+
     let voted = creatures.getVoted();
     let isVoted = req.user && voted.some(c => c._id == req.user?._id);
 
-    res.render('creatures/details', { ...creaturesData, isOwner, isVoted })
+    res.render('creatures/details', { ...creaturesData, isOwner, isVoted, creaturesOwner, creatureInfo, emails })
 });
 
 async function isOwner(req, res, next) {
@@ -70,8 +78,36 @@ router.get('/:id/delete', checkIsOwner, async (req, res) => {
 
         res.redirect('/creatures/all-posts');
     } catch (error) {
-        res.render('creatures/all-posts', { error: getErrorMessage(error) });
+        res.render('creatures/create', { error: getErrorMessage(error) });
     }
+
+});
+
+router.get('/:id/edit', async (req, res) => {
+    let creatures = await creaturesServices.getOne(req.params.id);
+    console.log(creatures);
+    res.render('creatures/edit', { ...creatures.toObject() })
+});
+
+router.post('/:id/edit', checkIsOwner, async (req, res) => {
+    try {
+        console.log(await creaturesServices.updateOne(req.params.id, req.body));
+
+        res.redirect(`/creatures/${req.params.id}/details`);
+    } catch(error) {
+        console.log(getErrorMessage(error));
+        res.render('creatures/create', { error: getErrorMessage(error) });
+    }
+
+});
+
+router.get('/:id/voted', isOwner, async (req, res) => {
+    let creatures = await creaturesServices.getOne(req.params.id);
+
+    creatures.voted.push(req.user);
+    await creatures.save();
+
+    res.redirect(`/creatures/${req.params.id}/details`);
 
 });
 
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -4,4 +4,8 @@ router.get('/', (req, res) => {
     res.render('home');
 });
 
+router.get('/profile', async (req, res) => {
+    res.render('profile')
+})
+
 module.exports = router;
